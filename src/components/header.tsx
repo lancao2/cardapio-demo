82b8b76deb097063d965fe6ddc46128eebfe19a3
lancao2 <lancao.af@gmail.com>
2024-04-05 17:08:58 -0300

    Feat: header component

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+export default function Header(): JSX.Element {
+  return (
+    <div className="pt-4">
+      <span className="text-[15px] text-[#272727] font-semibold pt-4 pl-4">
+        Olá, seja bem-vindo
+      </span>
+      <h2 className="text-[25px] text-[#272727] font-bold pt-3 pl-4 leading-7">
+        Prepare-se para uma experiência{" "}
+        <strong className="text-[#FFE500] font-bold">Inesquecível</strong>
+      </h2>
+    </div>
+  );
+}
